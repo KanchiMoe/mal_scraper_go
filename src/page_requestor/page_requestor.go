a7033e203cce5df22afc9c97b9002b6bba08b021
KanchiMoe <github@chiyo-chan.me>
2024-07-26 23:49:02 +0100

    added handling for deleted user account

--- src/page_requestor/page_requestor.go
+++ src/page_requestor/page_requestor.go
@@ -1,23 +1,19 @@
 package page_requestor
 
 import (
-	"fmt"
-
 	"github.com/antchfx/htmlquery"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/net/html"
 )
 
 func Request_handler(requested_url string) (html *html.Node, err error) {
+	log.Debug().Str("url", requested_url).Msg("Requesting url")
 	_, html, err = get_url(requested_url)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println("cccc")
-
 	return html, err
-
 }
 
 func get_url(requested_url string) (status_code int, html *html.Node, err error) {
@@ -32,7 +28,10 @@ func get_url(requested_url string) (status_code int, html *html.Node, err error)
 	}
 	defer response.Body.Close()
 
-	if response.StatusCode == 200 {
+	log.Trace().Int("code", response.StatusCode).Msg("Response code")
+
+	// actions based on response code
+	if response.StatusCode == 200 || response.StatusCode == 404 {
 		html, err = htmlquery.Parse(response.Body)
 		if err != nil {
 			log.Error().Err(err).Msg("Error when parsing response body")
