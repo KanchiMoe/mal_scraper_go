f514248f8273978956732dfcbbf15c0a2c444dde
KanchiMoe <github@chiyo-chan.me>
2024-07-29 03:23:37 +0100

    add count up user logic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	logic_users "github.com/kanchimoe/mal_scraper_go/src/logic/users"
 	"github.com/kanchimoe/mal_scraper_go/src/project_init"
 	"github.com/rs/zerolog/log"
 )
@@ -12,6 +13,8 @@ func main() {
 	if err != nil {
 		log.Panic().Err(err).Msg("Error initialising project")
 	}
+
+	logic_users.Count_up_1000(7354862)
 
 	fmt.Println("end")
 }
