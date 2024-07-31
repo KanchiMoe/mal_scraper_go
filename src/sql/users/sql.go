ec18aaf77751b47605a51914d00f86f8c3d160e9
KanchiMoe <github@chiyo-chan.me>
2024-07-31 01:02:33 +0100

    complete users sql

--- src/sql/users/sql.go
+++ src/sql/users/sql.go
@@ -5,9 +5,13 @@ import (
 	"errors"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kanchimoe/mal_scraper_go/src"
 	"github.com/kanchimoe/mal_scraper_go/src/project_structs"
+	changetracking "github.com/kanchimoe/mal_scraper_go/src/sql/change_tracking"
 	"github.com/rs/zerolog/log"
 )
+
+const error_location string = "sql/users"
 
 func Get_id_from_username(db_connection *pgxpool.Pool, username string) (query_results project_structs.Username_and_id) {
 	log.Trace().Str("username", username).Msg("SQL: search for username")
@@ -60,41 +64,55 @@ func Get_username_from_id(db_connection *pgxpool.Pool, user_id int) (query_resul
 func Add_user_id_to_db(db_connection *pgxpool.Pool, id int, username string) (err error) {
 	log.Trace().Int("id", id).Str("username", username).Msg("SQL: add user to database")
 
+	timestamp, err := src.Generate_timestamp()
+	if err != nil {
+		log.Error().Err(err).Str("location", error_location).Msg("Error calling generate timestamp")
+	}
+
 	// query
-	const sql_insert_new_user_id string = "INSERT INTO users (id, username) VALUES ($1, $2)"
+	const sql_insert_new_user_id string = "INSERT INTO users (id, username, last_interacted) VALUES ($1, $2, $3);"
 
 	// query results
-	_, err = db_connection.Exec(context.Background(), sql_insert_new_user_id, id, username)
+	_, err = db_connection.Exec(context.Background(), sql_insert_new_user_id, id, username, timestamp)
 	if err != nil {
-		return err
+		log.Error().Err(err).Msg("error query") //////////// TO DO, INVESTIGATE WHY NOT PROPOGATE -- TO TEST, REMOVE TIMESTAMP FIELDS
+		return err                              ////// ALSO!!! CHANGE TRACKING TABLE, TIMESTAMP FIELD IS UUID MUST CHANGE
 
 	} else {
 		log.Info().Str("username", username).Int("id", id).Msg("Username added")
+		err = changetracking.Add_to_change_tracking(db_connection, "USERS", id, "ID/USERNAME", "NULL", username)
+		if err != nil {
+			log.Error().Err(err).Str("location", error_location).Msg("Error passing to change tracking")
+		}
 	}
 
 	return nil
 }
 
-func Update_username_from_id(db_connection *pgxpool.Pool, username string, id int) (err error) {
-	log.Trace().Int("id", id).Str("username", username).Msg("SQL: update username")
+func Update_username_from_id(db_connection *pgxpool.Pool, new_username string, id int, old_username string) (err error) {
+	log.Trace().Int("id", id).Str("username", new_username).Msg("SQL: update username")
 
 	// query
 	var sql_query string = "UPDATE users SET username = $1 WHERE id = $2;"
 
 	// safeguard: check to make sure username is not just "[Deleted]"
-	if username == "[Deleted]" {
+	if new_username == "[Deleted]" {
 		err = errors.New("username is '[Deleted]'")
 		log.Error().Err(err).Msg("Username is '[Deleted]'")
 		return err
 	}
 
 	// query results
-	_, err = db_connection.Exec(context.Background(), sql_query, username, id)
+	_, err = db_connection.Exec(context.Background(), sql_query, new_username, id)
 	if err != nil {
 		return nil
 
 	} else {
-		log.Info().Str("new username", username).Int("id", id).Msg("Username updated")
+		log.Info().Str("new", new_username).Int("id", id).Msg("Username updated")
+		err = changetracking.Add_to_change_tracking(db_connection, "USERS", id, "ID/USERNAME", old_username, new_username)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
