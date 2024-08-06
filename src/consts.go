d8d8bb32afd03154c414b0eda22f6f792007bad2
KanchiMoe <github@chiyo-chan.me>
2024-08-06 04:36:30 +0100

    add genre constant

--- src/consts.go
+++ src/consts.go
@@ -5,6 +5,7 @@ const ROOT_URL string = "https://myanimelist.net/"
 const PEOPLE_URL string = "people/"          // https://myanimelist.net/people/000/
 const CHARACTER_URL string = "character/"    // https://myanimelist.net/character/000/
 const COMPANY_URL string = "anime/producer/" // https://myanimelist.net/anime/producer/000
+const GENRE_URL string = "anime/genre/"      // https://myanimelist.net/anime/genre/000
 
 // users
 const PROFILE_URL string = "profile/" // https://myanimelist.net/profile/Name
